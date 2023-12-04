5e171f13723f2ccf3467f8d1e04fb5225e061ecc
ryan-mcdowell <ryan-mcdowell@outlook.com>
2023-12-03 21:17:11 -0800

    Another attempt at pokemon script

--- scripts/pokemon/setup.sh
+++ scripts/pokemon/setup.sh
@@ -7,20 +7,20 @@ BASE_DIR="/content/EE562_AI_Project/scripts/pokemon"
 cd "$BASE_DIR"
 
 # Remove existing dataset
-rm -rf "$BASE_DIR/../../datasets/pokemon_pix2pix_dataset"
+rm -rf "../../datasets/pokemon_pix2pix_dataset"
 
 # Download and move the dataset
 kaggle datasets download -d norod78/sketch2pokemon
-mv -f sketch2pokemon.zip "$BASE_DIR/../../datasets/"
+mv -f sketch2pokemon.zip "../../datasets/"
 
 # Unzip the dataset
-cd "$BASE_DIR/../../datasets/"
+cd "../../datasets/"
 unzip sketch2pokemon.zip
 rm -f sketch2pokemon.zip
-cp -f "$BASE_DIR/organize.sh" "$BASE_DIR/make_dirs.sh" pokemon_pix2pix_dataset
+cp -f "$BASE_DIR/organize.sh" "$BASE_DIR/make_dirs.sh" "pokemon_pix2pix_dataset"
 
 # Change to the dataset directory
-cd "$BASE_DIR/pokemon_pix2pix_dataset"
+cd "pokemon_pix2pix_dataset"
 ./make_dirs.sh
 ./organize.sh
 
