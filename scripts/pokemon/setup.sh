2d641e8e4d4c541d19474934157f14fe7bfad7ab
ryan-mcdowell <ryan-mcdowell@outlook.com>
2023-12-03 21:09:12 -0800

    Make pokemon setup script more modular

--- scripts/pokemon/setup.sh
+++ scripts/pokemon/setup.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-rm -rf ../../datasets/pokemon_pix2pix_dataset
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+# Remove existing dataset
+rm -rf "$SCRIPT_DIR/../../datasets/pokemon_pix2pix_dataset"
+
+# Download and move the dataset
 kaggle datasets download -d norod78/sketch2pokemon
-mv -f sketch2pokemon.zip ../../datasets/
+mv -f "$SCRIPT_DIR/sketch2pokemon.zip" "$SCRIPT_DIR/../../datasets/"
 
-cd ../../datasets/
+# Unzip the dataset
+cd "$SCRIPT_DIR/../../datasets/"
 unzip sketch2pokemon.zip
 rm -f sketch2pokemon.zip
-cp -f ../scripts/pokemon/organize.sh ../scripts/pokemon/make_dirs.sh pokemon_pix2pix_dataset
+cp -f "$SCRIPT_DIR/organize.sh" "$SCRIPT_DIR/make_dirs.sh" pokemon_pix2pix_dataset
 
-cd pokemon_pix2pix_dataset
+# Change to the dataset directory
+cd "$SCRIPT_DIR/pokemon_pix2pix_dataset"
 ./make_dirs.sh
 ./organize.sh
 
-cd ../../
-python datasets/combine_A_and_B.py --fold_A ./datasets/pokemon_pix2pix_dataset/A --fold_B ./datasets/pokemon_pix2pix_dataset/B --fold_AB ./datasets/pokemon_pix2pix_dataset/AB
+# Go back to the original directory and run the Python script
+cd "$SCRIPT_DIR/../.."
+python "$SCRIPT_DIR/datasets/combine_A_and_B.py" --fold_A "$SCRIPT_DIR/datasets/pokemon_pix2pix_dataset/A" --fold_B "$SCRIPT_DIR/datasets/pokemon_pix2pix_dataset/B" --fold_AB "$SCRIPT_DIR/datasets/pokemon_pix2pix_dataset/AB"
