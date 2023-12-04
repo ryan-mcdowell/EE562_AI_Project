8876bf78398f433f19d2cd62d166704bd8f9d875
ryan-mcdowell <ryan-mcdowell@outlook.com>
2023-12-03 21:26:16 -0800

    Another pokemon script

--- scripts/pokemon/setup.sh
+++ scripts/pokemon/setup.sh
@@ -17,13 +17,17 @@ mv -f sketch2pokemon.zip "../../datasets/"
 cd "../../datasets/"
 unzip sketch2pokemon.zip
 rm -f sketch2pokemon.zip
-mv -f pokemon_pix2pix_dataset "$BASE_DIR/../datasets/"
+
+# Move the scripts
+cp -f $BASE_DIR/make_dirs.sh pokemon_pix2pix_dataset
+cp -r $BASE_DIR/organize.sh pokemon_pix2pix_dataset
 
 # Change to the dataset directory
-cd "$BASE_DIR/../datasets/pokemon_pix2pix_dataset"
+cd "$BASE_DIR/../../datasets/pokemon_pix2pix_dataset"
 ./make_dirs.sh
 ./organize.sh
 
 # Go back to the original directory and run the Python script
-cd "$BASE_DIR/../datasets"
+cd "../.."
 python "combine_A_and_B.py" --fold_A "pokemon_pix2pix_dataset/A" --fold_B "pokemon_pix2pix_dataset/B" --fold_AB "pokemon_pix2pix_dataset/AB"
+cd ".."
